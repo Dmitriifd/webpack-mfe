880fbebc63f27d1379c6e5597e5fe0b3e1a5b01d
Dmitrii <idimka9@gmail.com>
2025-02-08 16:33:27 +0500

    test lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
 import './styles/global.scss';
-// import { ReactComponent as Logo } from './assets/logo.svg';
-import { ReactComponent as LogoComponent } from './assets/logo.svg';
 import logoUrl from './assets/logo.svg';
+import { ReactComponent as LogoComponent } from './assets/logo.svg';
 
 const App = () => {
   return (
     <div>
       <h1>App Test </h1>
       <LogoComponent width={50} height={50} />
+
       <img src={logoUrl} alt="logo" width={50} height={50} />
     </div>
   );
